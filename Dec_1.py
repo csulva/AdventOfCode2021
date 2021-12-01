bce89c7682f34ffa2c0d77aae05de5155bb1fef5
csulva <chrissulva1@gmail.com>
2021-12-01 17:03:45 -0500

    complete day 1

--- Dec_1.py
+++ Dec_1.py
@@ -1,6 +1,10 @@
+# Part 1
+# How many measurements are larger than the previous measurement?
+
 with open('Dec_1.txt', 'r') as infile:
     input = infile.readlines()
 
+# Converting to ints
 new_list = []
 for x in input:
     x = int(x)
@@ -10,6 +14,27 @@ increase = 0
 index = 0
 for x in new_list:
     if x > new_list[index - 1]:
+        increase += 1
+    index += 1
+
+# print(increase)
+
+# Part 2
+# Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+
+index = 0
+list_of_sum = []
+for x in new_list:
+    sum = new_list[index] + new_list[index + 1] + new_list[index + 2]
+    list_of_sum.append(sum)
+    index += 1
+    if index == len(new_list) - 2:
+        break
+
+increase = 0
+index = 0
+for x in list_of_sum:
+    if x > list_of_sum[index - 1]:
         increase += 1
     index += 1
 
